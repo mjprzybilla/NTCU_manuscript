852d0aac4bb1143d57e6f56b3070d0eb800eaac6
ibordeu <ibordeu@gmail.com>
2025-04-02 17:19:19 -0300

    update

--- figures/figure2/Plot_Figure_2EFG.m
+++ figures/figure2/Plot_Figure_2EFG.m
@@ -1,7 +1,7 @@
 %% Plot Figure 2 E,F,G (experimental data only)
 % directory with void data
 
-root_path = '';
+root_path = 'data';
 
 % datasets
 dset = {}; lstyle = {};
@@ -83,5 +83,50 @@ for nset = 1:3
         x = [70,150]; y = 9./x;
         plot(x,y,'k-','linewidth',2,'handlevisibility','off')
     end
+
+    if nset == 2
+        plot_theory([root_path,'/neutral_sim_params.mat'],[root_path,'/neutral_data_holes.mat'],93)
+    elseif nset ==3
+        plot_theory([root_path,'/non_neutral_sim_params.mat'],[root_path,'/non_neutral_data_holes.mat'],130)
+
+    end
 end
 set(gcf,'Color','w')
+
+function plot_theory(sim_params_file,data_holes_file,tplot)
+        min_hole_size = 2;
+        sim_params = load(sim_params_file); sim_params = sim_params.sim_params;
+        data_holes = load(data_holes_file); data_holes = data_holes.x;
+
+        nrepeats = sim_params('nrepeats');
+        factor = sim_params('factor');
+        t_max = sim_params('t_max');
+        N = ceil(t_max/factor);
+
+        cdfs = [];
+        all_hole_areas = [];
+        for nrep = 1:nrepeats
+            hole_areas = data_holes(nrep).area{tplot};
+            hole_areas(hole_areas<min_hole_size) = [];
+            [cdf,bincents] = compute_ccdf(hole_areas,min_hole_size);
+            cdfs(nrep,:) = cdf;
+        end
+        m = mean(cdfs);
+        stdev = std(cdfs,1);
+        tplot = size(cdfs,1);
+        minvals = m-stdev;
+        hold on
+        plot(bincents,m)
+        plot(bincents,m+stdev,'-k','handlevisibility','off')
+        plot(bincents,m-stdev,'-k','handlevisibility','off')
+        hold off
+end
+
+function [cdf,bincents] = compute_ccdf(clone_sizes,min_hole_size)
+binedges = min_hole_size-0.5:1:5*10^5;
+bincents = (binedges(2:end) + binedges(1:end - 1))/2;
+% compute pdf
+Nnorm = histcounts(clone_sizes,binedges,'Normalization','pdf');
+% compute cdf
+cdf = 1 - cumsum(Nnorm)./sum(Nnorm);
+end
--- figures/figure2/readme.md
+++ figures/figure2/readme.md
@@ -1,8 +1,12 @@
 # FIGURE 2
 
-This GitHub repository contains the files to reproduce experimental curves shown in Figure 2E,F and G.
+This GitHub repository contains the files to reproduce Figures 2E,F and G.
 
 Content:
 ```
-└─ area_hole_cells.mat: corresponding to the area of voids (units of cells) obtained for the 3 conditions: two controls, and long term NTCU treament, measured at 24 weeks.
 └─ Plot_Figure_2EGF: load the raw data in data_voidarea_cells.mat, computes and plot the chefs shown in Fig 2E, F and G. Curves for the theoretical model can be obtained from running the scripts in Models/neutral_model (for 2F) and Models/non_neutral_model (for 2G).
+└─ data
+| └─ area_hole_cells.mat: area of voids (units of cells) obtained for the 3 conditions: two controls, and long term NTCU treatment, measured at 24 weeks (used for reproducing Figs. 2E,F and G).
+| └─ neutral_sim_params.mat and neutral_data_holes.mat: simulation parameters and area of voids (units of cells) obtained from 200 repeats of the neutral model (used for reproducing Fig. 2F).
+| └─ non_neutral_sim_params.mat and non_neutral_data_holes.mat: simulation parameters and area of voids (units of cells) obtained from 200 repeats of the non neutral model (used for reproducing Fig. 2G).
+```
